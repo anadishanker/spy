9ad6e2a74739c76a871671ecd9e3590c9a8b28b3
anadishanker <shankeranadi@gmail.com>
2018-03-20 14:38:43 +0530

    this is first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello")
+print ('What\'s up?')
+print ('let\'s get started')
+spy_name=input("whats your name?")
+print ('welcome ' + spy_name + '. glad to have you backwith us. ')
+spy_salutation = input("what should we call you (Mr. or Ms.) ?")
+spy_salutation + " " + spy_name
+spy_name = spy_salutation + " " + spy_name
+print(spy_name)
+
