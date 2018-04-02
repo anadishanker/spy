2e0636fd43fc9e4fecb7aef47a5bccd795655c76
anadishanker <shankeranadi@gmail.com>
2018-04-02 15:49:14 +0530

    fifth commit

--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,1 @@
-name="Anadi"
-spy_salutation="Mr."
-full_name=spy_salutation + " " + name
-spy_age=21
-spy_rating=1
-status = "you are in..."
+#####name="Anadi shanker"#####spy_salutation="Mr."#####full_name=spy_salutation + " " + name#####spy_age=21#####spy_rating=4#####status = "You are in..."spy = {"Name": "Anadi shanker", "Salutation": "Mr.", "age": 21, "Rating": 4.5, "Status": "You are in...."}
